aa7f5e32aa89758cfe06da3c20e368b2298acd7e
Greg Herpel <gherpel2@jhu.edu>
2022-12-01 12:17:45 -0500

    Add project files.

---
+++ adventpuzzle/Program.cs
@@ -0,0 +1,25 @@
+﻿var text = System.IO.File.ReadAllLines("day1.txt");
+
+var running = 0;
+var elfTotal = 1;
+var elves = new Dictionary<int, int>();
+foreach (var line in text)
+{
+    var lineAmount = 0;
+    if (int.TryParse(line, out lineAmount) && lineAmount != 0)
+    {
+        running+=lineAmount;
+    }
+    else
+    {
+        elves.Add(elfTotal, running);
+        elfTotal++;
+        running = 0;
+    }
+}
+
+var greatest = elves.OrderByDescending(k => k.Value).Take(1).Select(k => k.Value).Sum();
+var topThree = elves.OrderByDescending(k => k.Value).Take(3).Select(k=> k.Value).Sum();
+
+Console.WriteLine($@"Greatest: {greatest}");
+Console.WriteLine($@"TOP3 Total: {topThree}");
