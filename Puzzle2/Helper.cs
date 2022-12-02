09771845087d512fc23176637715ccf7f30a1b1f
Greg Herpel <gherpel2@jhu.edu>
2022-12-02 00:55:59 -0500

    puzzle2

---
+++ Puzzle2/Helper.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Puzzle2
+{
+    public static class Helper
+    {
+        public enum RPS
+        {
+            Rock = 1,
+            Paper = 2,
+            Scissors = 3
+        }
+
+        public enum Outcome
+        {
+            Lose = 0,
+            Draw = 3,
+            Win = 6
+        }
+
+        public static int CalcPoints(RPS opp, RPS you)
+        {
+            var points = (int)you;
+            if ((opp == RPS.Rock && you == RPS.Paper) || (opp == RPS.Paper && you == RPS.Scissors) ||
+                (opp == RPS.Scissors && you == RPS.Rock))
+            {
+                points += (int)Outcome.Win;
+            }
+            else if (opp == you)
+            {
+                points += (int)Outcome.Draw;
+            }
+
+            return points;
+        }
+
+        public static int CalcPoints(RPS opp, Outcome you)
+        {
+            var points = (int)you;
+            if (you == Outcome.Draw)
+            {
+                points += (int)opp;
+            }
+            else if (you == Outcome.Win)
+            {
+                points += ((int)opp % 3) + 1;
+            }
+            else
+            {
+                points += ((int)opp + 1) % 3 + 1;
+            }
+
+            return points;
+        }
+
+        public static RPS MapLetters(char letter)
+        {
+            return letter switch
+            {
+                'A' or 'X' => RPS.Rock,
+                'B' or 'Y' => RPS.Paper,
+                _ => RPS.Scissors
+            };
+        }
+
+        public static Outcome MapOutcome(char letter)
+        {
+            return letter switch
+            {
+                'X' => Outcome.Lose,
+                'Y' => Outcome.Draw,
+                _ => Outcome.Win
+            };
+        }
+    }
+}
---
+++ Puzzle2/Program.cs
@@ -0,0 +1,24 @@
+﻿using Puzzle2;
+
+var text = System.IO.File.ReadAllLines("day2.txt");
+
+var points = 0;
+foreach (var line in text)
+{
+    var opp = Helper.MapLetters(line[0]);
+    var you = Helper.MapLetters(line[2]);
+    points += Helper.CalcPoints(opp, you);
+}
+
+Console.WriteLine($"pt1: {points}");
+
+points = 0;
+foreach (var line in text)
+{
+    var opp = Helper.MapLetters(line[0]);
+    var you = Helper.MapOutcome(line[2]);
+    points += Helper.CalcPoints(opp, you);
+}
+
+Console.WriteLine($"pt2: {points}");
+
