65f2085a7542f2cc75ecbdf0bef1fda4830b3d69
Greg Herpel <gherpel2@jhu.edu>
2022-12-03 01:22:45 -0500

    day3 ugly POS too late at night code

---
+++ Puzzle3/Program.cs
@@ -0,0 +1,41 @@
+﻿using Puzzle3;
+
+var text = System.IO.File.ReadAllLines("day3.txt");
+
+var total = 0;
+
+foreach (var line in text)
+{
+    var left = line.Substring(0, line.Length / 2);
+    var right = line.Substring(line.Length / 2);
+    
+    foreach (var item in left)
+    {
+        if (right.Contains(item))
+        {
+            total += (int)((Helper.alphabet)Enum.Parse(typeof(Helper.alphabet), item.ToString()))+1;
+            break;
+        }
+    }
+}
+
+Console.WriteLine($"p1: {total}");
+
+total = 0;
+for(int i = 0; i < text.Length; i++)
+{
+    var one = text[i];
+    var two = text[i += 1];
+    var three = text[i += 1];
+
+    foreach (var item in one)
+    {
+        if (two.Contains(item) && three.Contains(item))
+        {
+            total += (int)((Helper.alphabet)Enum.Parse(typeof(Helper.alphabet), item.ToString())) + 1;
+            break;
+        }
+    }
+}
+
+Console.WriteLine($"p2: {total}");
---
+++ Puzzle3/Rucksack.cs
@@ -0,0 +1,8 @@
+﻿namespace Puzzle3
+{
+    public class Rucksack
+    {
+        public List<Helper.alphabet> left = new List<Helper.alphabet>();
+        public List<Helper.alphabet> right = new List<Helper.alphabet>();
+    }
+}
