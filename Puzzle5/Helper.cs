dbfd4944052678e528bd36e598794050eb48b935
Greg Herpel <gherpel2@jhu.edu>
2022-12-05 01:14:55 -0500

    day 5

---
+++ Puzzle5/Helper.cs
@@ -0,0 +1,40 @@
+﻿namespace Puzzle5
+{
+    public static class Helper
+    {
+        public static List<Stack<char>> CreateStacks(int size)
+        {
+            var ret = new List<Stack<char>>();
+            for (int i = 0; i < size; i++)
+            {
+                ret.Add(new Stack<char>());
+            }
+            return ret;
+        }
+
+        public static int SetStacks(int index, string[] text, List<Stack<char>> stacks)
+        {
+            var maxIndex = 0;
+            if (!text[index].StartsWith(" 1"))
+            {
+                maxIndex = SetStacks(index + 1, text, stacks);
+            }
+            else
+            {
+                return index;
+            }
+
+            var line = text[index];
+            var itemLoc = line.IndexOf('[');
+            while (itemLoc >= 0)
+            {
+                var crate = line[itemLoc + 1];
+                var stackLoc = itemLoc / 4;
+                stacks[stackLoc].Push(crate);
+                itemLoc = line.IndexOf('[', itemLoc+1);
+            }
+
+            return maxIndex;
+        }
+    }
+}
---
+++ Puzzle5/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Linq.Expressions;
+using System.Text.RegularExpressions;
+using Puzzle5;
+
+var text = System.IO.File.ReadAllLines("day5.txt");
+
+//set up stacks
+
+var index = 0;
+var line = text[index];
+var stacks = Helper.CreateStacks(line.Length/4 + 1);
+
+var instStart = Helper.SetStacks(index, text, stacks) + 1;
+
+var readRegex = new Regex(@"move (\d+) from (\d+) to (\d+)");
+
+for (int i = instStart; i < text.Length; i++)
+{
+    var match = readRegex.Match(text[i]);
+    if (match.Success)
+    {
+        var move = int.Parse(match.Groups[1].Value);
+        var from = int.Parse(match.Groups[2].Value) - 1;
+        var to = int.Parse(match.Groups[3].Value) - 1;
+
+        //p1
+        /*
+        for (int j = 0; j < move; j++)
+        {
+            var crate = stacks[from].Pop();
+            stacks[to].Push(crate);
+        }
+        */
+
+        //p2
+        var crates = new Stack<char>();
+        for (int j = 0; j < move; j++)
+        {
+            crates.Push(stacks[from].Pop());
+        }
+
+        while (crates.Count > 0)
+        {
+            stacks[to].Push(crates.Pop());
+        }
+    }
+}
+
+var output = string.Empty;
+foreach (var stack in stacks)
+{
+   output += stack.Pop();
+}
+
+Console.WriteLine($"{output}");
